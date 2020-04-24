d18e69b58b7bda6c9d4b2b891c0c598d494bb558
Caleb Spring <cspring@zixcorp.com>
2020-04-24 13:51:51 -0500

    caught up to end of lesson 3/video 65

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-import './menu-item.styles.scss'
+import './menu-item.styles.scss';
 
-const MenuItem = (props) => (
-    <div style={{
-            backgroundImage: `url(${props.imageUrl})`
-        }} className={`${props.size} menu-item`} 
-    >
-        <div className='content'>
-            <h1 className='title'>{props.title}</h1>
-            <span className='subtitle'>SHOP NOW</span>                    
-        </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-)
+  </div>
+);
 
 export default MenuItem;
